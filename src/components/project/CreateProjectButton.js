62271411f6c1ae815cbfec13a892135660ea0762
Saurabh Kumar <saurabhk15@outlook.com>
2020-06-30 03:33:34 +0530

    created router- functional-component-action

---
+++ src/components/project/CreateProjectButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+
+const CreateProjectButton = () => {
+  return (
+    <React.Fragment>
+      <Link to="/addProject">
+        <Button href="ProjectForm.html" className="btn btn-lg btn-info">
+          Create a Project
+        </Button>
+      </Link>
+    </React.Fragment>
+  );
+};
+
+export default CreateProjectButton;
