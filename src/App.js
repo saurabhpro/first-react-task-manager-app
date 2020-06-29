fedbdbea8fdddc8e5225f52f58db59ccbf282b54
Saurabh Kumar <saurabhk15@outlook.com>
2020-06-30 00:10:41 +0530

    add dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Dashboard/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
