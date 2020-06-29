6d0173f284cd94d3e1342fc5e003c4d3eaae15de
Saurabh Kumar <saurabhk15@outlook.com>
2020-06-30 00:23:24 +0530

    add more components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Dashboard/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Welcome to Saurabh's First React App</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Welcome to Saurabh's First React App</h1>
+        
+      <Dashboard/>
+      </div>
   );
 }
 
