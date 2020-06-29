68607a3957f76d659fb561274d59925f55807312
Saurabh Kumar <saurabhk15@outlook.com>
2020-06-30 01:27:55 +0530

    add react-bootstrap supported dashboard navbar and bootstrap project item

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import './App.css';
-import Dashboard from './components/Dashboard';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Saurabh's First React App</h1>
-        
-      <Dashboard/>
-      </div>
+      <Header />
+      <Dashboard />
+    </div>
   );
 }
 
