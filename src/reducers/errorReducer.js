a5d90caed2bc3efaf1bce79adec87d6a75b4eb75
Saurabh Kumar <saurabhk15@outlook.com>
2020-07-01 00:14:53 +0530

    arg order didn't work

--- src/reducers/errorReducer.js
+++ src/reducers/errorReducer.js
@@ -3,7 +3,7 @@ import { GET_ERRORS } from "../actions/types";
 const initialState = {};
 
 // function to return the payload for our action type
-export default function (action, state = initialState) {
+export default function (state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
       return action.payload;
