1398eaa213f29f1eb6b6a74a9747212f67b67f3b
Saurabh Kumar <saurabhk15@outlook.com>
2020-07-05 03:57:20 +0530

    define basic type constants for backlog

---
+++ src/reducers/backlogReducer.js
@@ -0,0 +1,34 @@
+import {
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK,
+} from "../actions/types";
+
+const initialState = {
+  projectTasks: [],
+  projectTask: {},
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_BACKLOG:
+      return {
+        ...state,
+        projectTasks: action.payload,
+      };
+    case GET_PROJECT_TASK:
+      return {
+        ...state,
+        projectTask: action.payload,
+      };
+
+    case DELETE_PROJECT_TASK:
+      return {
+        ...state,
+        //TODO filter out deleted tasks from redux
+      };
+
+    default:
+      return state;
+  }
+}
