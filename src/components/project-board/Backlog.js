838025d7d1005652e7da2ac21bee7309d959b0a4
Saurabh Kumar <saurabhk15@outlook.com>
2020-07-05 13:46:25 +0530

    move project backlog tasks to its own card stack

--- src/components/project-board/Backlog.js
+++ src/components/project-board/Backlog.js
@@ -15,30 +15,54 @@ class Backlog extends Component {
       <ProjectTask key={projectTask.id} projectTask={projectTask} />
     ));
 
+    // group tasks into three decks
+    let toDoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+
+    tasks.forEach((task) => {
+      switch (task.props.projectTask.status) {
+        case "TO_DO":
+          toDoItems.push(task);
+          break;
+        case "IN_PROGRESS":
+          inProgressItems.push(task);
+          break;
+        case "DONE":
+          doneItems.push(task);
+          break;
+        default:
+          console.error("Unknown Card Deck : " + task.props.projectTask.status);
+          break;
+      }
+    });
+
     return (
       <CardDeck>
         <Card bg="dark" text="white" className="text-center p-2">
           <Card.Header as="h5" className="mb-2">
             TO DO
           </Card.Header>
-          {tasks}
+          {toDoItems}
         </Card>
 
         <Card
           md={{ offset: 1 }}
           bg="warning"
-          text="white"
+          text="light"
           className="text-center p-2"
         >
           <Card.Header as="h5" className="mb-2">
             In Progress
           </Card.Header>
+          {inProgressItems}
         </Card>
 
-        <Card bg="success" text="white" className="text-center p-2">
+        <Card bg="success" text="light" className="text-center p-2">
           <Card.Header as="h5" className="mb-2">
             Done
           </Card.Header>
+          {doneItems}
         </Card>
       </CardDeck>
     );
--- src/components/project-board/project-task/ProjectTask.js
+++ src/components/project-board/project-task/ProjectTask.js
@@ -14,30 +14,32 @@ class ProjectTask extends Component {
     const createdAt = moment(projectTask).format("MMMM Do YYYY");
 
     return (
-      <Card text="dark" className="mb-2">
-        <Card.Header className="text-primary">
-          ID: {projectTask.projectSequence} & Priority: {projectTask.priority}
-        </Card.Header>
-        <Card.Body>
-          <Card.Title>{projectTask.summary}</Card.Title>
-          <Card.Text className="text-truncate">
-            {projectTask.acceptanceCriteria}
-          </Card.Text>
+      <div className="row-sm-3">
+        <Card text="dark" className="mb-2 mh-20">
+          <Card.Header className="text-primary">
+            ID: {projectTask.projectSequence} & Priority: {projectTask.priority}
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>{projectTask.summary}</Card.Title>
+            <Card.Text className="text-truncate">
+              {projectTask.acceptanceCriteria}
+            </Card.Text>
 
-          <ButtonGroup>
-            {" "}
-            <Button variant="primary" className="mr-2">
-              <FontAwesomeIcon icon="edit" /> View / Update
-            </Button>
-            <Button variant="danger">
-              <FontAwesomeIcon icon="minus-square" /> Delete
-            </Button>
-          </ButtonGroup>
-        </Card.Body>
-        <Card.Text>
-          <small className="text-muted">Created on {createdAt}</small>
-        </Card.Text>
-      </Card>
+            <ButtonGroup>
+              {" "}
+              <Button variant="primary" className="mr-2">
+                <FontAwesomeIcon icon="edit" /> View / Update
+              </Button>
+              <Button variant="danger">
+                <FontAwesomeIcon icon="minus-square" /> Delete
+              </Button>
+            </ButtonGroup>
+          </Card.Body>
+          <Card.Text>
+            <small className="text-muted">Created on {createdAt}</small>
+          </Card.Text>
+        </Card>
+      </div>
     );
   }
 }
